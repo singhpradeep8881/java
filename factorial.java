7f2772c191e4dcc7946d6bf3c6902b406ce92306
Pradeep <singhpradeepkumar8881@gmail.com>
2024-10-02 12:13:01 +0530

    factorial program

--- factorial.java
+++ factorial.java
@@ -1,3 +1,19 @@
-public class factorial {
-    
+import java.util.Scanner;
+
+public class factorial
+{
+    public static void main(String a[]){
+        Scanner on = new Scanner (System.in);
+        int n= on.nextInt();
+        
+        factorial ob=new factorial();
+        ob.fac(n);
+    }
+    void fac(int n){
+        int fact=1;
+        for (int i =1;i<=n;i++){
+            fact =fact*i;
+        }
+        System.out.println("FActorial = "+ fact);
+    }
 }
